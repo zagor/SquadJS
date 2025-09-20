67f892f295df924fa768f6a0ab694908d7f8dc76
Björn Stenberg <bjorn@haxx.se>
2025-09-20 10:14:12 +0200

    Clean up player prefix regex

--- squad-server/log-parser/player-prefix.js
+++ squad-server/log-parser/player-prefix.js
@@ -2,11 +2,12 @@ import { iterateIDs, lowerID } from 'core/id-parser';
 
 export default {
   regex:
-  /^\[([0-9.:-]+)]\[([ 0-9]*)]LogSquadCommon: SQCommonStatics Check Permissions, UniqueId:([0-9a-f]+)/,
+  /^\[([\d.:-]+)\]\[([ \d]*)\]LogSquadCommon: SQCommonStatics Check Permissions, UniqueId:(\w+)/,
   onMatch: (args, logParser) => {
     const eosID = args[3];
-    let player = logParser.eventStore.players[eosID];
-    if (!player || !player.playerSuffix || player.seen) return;
+    const player = logParser.eventStore.players[eosID];
+    if (!player || !player.playerSuffix || player.seen)
+      return;
     player.seen = true;
 
     const data = {
